7d2092c019354a22349af0231273168c97350310
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-05-05 17:57:06 +0200

    changes to stay at /bookmarks route after saving

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -7,7 +7,9 @@ exports.getAllBookmarks = (req, res) => {
         .exec()
         .then(bookmarks => {
             res.render("bookmarks", {
-                bookmarks: bookmarks
+                bookmarks: bookmarks,
+                savedData: false,
+                newBookmark: null
             });
         })
         .catch((error) => {
@@ -19,13 +21,6 @@ exports.getAllBookmarks = (req, res) => {
         });
 };
 
-// Wird wegen getAllBookmarks() nicht mehr benötigt, meine ich
-/*
-exports.sendBookmarks = (req, res) => {
-    res.render("bookmarks");
-};
-*/
-
 exports.saveBookmarks = (req, res) => {
     let newBookmark = new Bookmark({
         title: req.body.title,
@@ -34,7 +29,11 @@ exports.saveBookmarks = (req, res) => {
     newBookmark
         .save()
         .then(() => {
-            res.render("done");
+            res.render("bookmarks", {
+                bookmarks: null,
+                savedData: true,
+                newBookmark: newBookmark
+            });
         })
         .catch((error) => {
             res.send(error);
