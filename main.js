7a942389cdba02a75d1ae6abe8910a9343999dcf
Nima Azimihashemi <s0557318@htw-berlin.de>
2021-05-13 14:41:40 +0200

    added mongoDB commandline in main

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ const express = require("express"),
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
+    process.env.MONGODB_URI ||
     "mongodb://localhost:27017/yapp-db",
     {useNewUrlParser: true, useFindAndModify: false, useCreateIndex:true, useUnifiedTopology: true}
 );
