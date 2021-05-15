919ea61843409b97331ee1c12fcab61453721de3
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-05-15 17:43:49 +0200

    installed supertest and created some smoke / endpoitn tests

---
+++ __tests__/server.test.js
@@ -0,0 +1,27 @@
+const app = require("../app");
+const supertest = require("supertest");
+
+// creates request to test endpoints
+const request = supertest(app);
+
+describe("Execute some smoke tests", () => {
+  
+    // Sends GET request to /test endpoint
+    it("Gets the test endpoint", async done => {
+        const res = await request.get("/test");
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe('pass!');
+        done()
+
+    });
+    
+    // Sends GET request to /test endpoint
+    it("Gets the test endpoint", async done => {
+        const res = await request.get("/test");
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe('pass!');
+        done()
+
+    });
+  
+  });
